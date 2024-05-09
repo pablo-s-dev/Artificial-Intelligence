d8e36a7028e9cf9754da613576e6521e6e86478e
pablo-s-dev <ps.login.username@gmail.com>
2024-05-09 11:27:50 -0300

    feat: headers && grredy algo

--- search_algorithms.py
+++ search_algorithms.py
@@ -52,7 +52,6 @@ def uniform_cost_search(start_state: Hashable, target_state: Hashable, successor
 
     while frontier:
 
-        (f, cost, state)
 
         root_state, root_cost = frontier.popitem()
 
@@ -266,4 +265,139 @@ def breadth_first_search(start_state: Hashable, target_state: Hashable, successo
                 'expansions': expansions
             }
         
+    return
+
+def greedy_search(start_state: Hashable, target_state: Hashable, successor_fun: Callable, step_by_step: bool = False, ):
+
+    print(f"Searching from {start_state} to {target_state}.")
+
+    """
+    Uniform Cost Search Algorithm
+
+    Args:
+
+    start_state: any hashable object
+    target_state: any hashable object
+    successor_fun: a function that returns the successors of a given state
+    step_by_step: a boolean flag to return the search steps
+
+    Returns:
+
+    A dictionary with the following keys
+
+    path: a list of states from start to target
+    cost: the cost of the path
+    expansions: the number of expanded nodes
+    depth: the depth of the path
+    frontier: the states in the frontier
+    state: the target state
+    explored: the states explored
+    action: the action taken to reach the target state
+
+    """
+
+    
+    # Keeps track of the shortest known distances and predecessors
+    node_info = {
+        start_state: {
+            'cost': 0,
+            'prev': None
+        }
+    }
+
+    expansions = 0
+
+    action = ''
+
+    # Only for logging purposes, not used in the algorithm, we use node_info instead
+    explored = set()
+    
+    frontier = heapdict()
+
+    frontier[start_state] = 0
+
+    while frontier:
+
+
+        root_state, root_heuristic = frontier.popitem()
+        root_cost = node_info[root_state]['cost']
+
+        explored.add(root_state)
+
+        if root_state == target_state:
+
+            path = []
+
+            node_state = root_state
+
+            while node_state:
+
+                path.append(node_state)
+                node_state = node_info[node_state].get('prev', None)
+
+            yield {
+                'path': list(reversed(path)),
+                'cost': root_cost,
+                'depth': len(path) - 1,
+                'frontier': list(frontier.keys()),
+                'state': root_state,
+                'explored': explored,
+                'expansions': expansions,
+                'action': '',
+            }
+            return
+
+        # Expanding the root tree
+        children = successor_fun(root_state)
+
+        for child_state, child_cost, child_heuristic in children:
+
+            cur_cost = root_cost + child_cost
+
+            # new or better path to this node
+            if child_state not in node_info or node_info[child_state]['cost'] > cur_cost:
+
+                frontier[child_state] = child_heuristic
+
+                node_info[child_state] = {
+                    'cost': cur_cost,
+                    'prev': root_state,
+                }
+
+        expansions += 1
+        
+
+        if step_by_step:
+
+            next_state, next_cost = frontier.peekitem()
+
+            if next_state:
+                action = f"Visit node {next_state} with cost {next_cost}."
+            else:
+                action = ""
+
+            path = []
+
+            node_state = root_state
+
+            while node_state:
+
+                path.append(node_state)
+                node_state = node_info[node_state].get('prev', None)
+
+            depth = len(path) - 1
+
+
+            yield {
+                'path': list(reversed(path)),
+                'cost': root_cost,
+                'depth': depth,
+                'frontier': list(frontier.keys()),
+                'action': action,
+                'state': root_state,
+                'explored': explored,
+                'expansions': expansions
+            }
+
+
     return
